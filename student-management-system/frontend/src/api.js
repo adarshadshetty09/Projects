066968190eade2bdeb3f77846110bdf07bffeda9
adarshadshetty09 <adarshadshetty09@gmail.com>
2025-12-26 18:59:56 +0530

    project deleted

--- student-management-system/frontend/src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-// Change this URL when you switch backends (Node: 5000, Python: 8000, etc.)
-const API_BASE_URL = 'http://localhost:5000/api';
-
-export const submitMarks = (data) => axios.post(`${API_BASE_URL}/marks`, data);
-export const getStudentMarks = (name) => axios.get(`${API_BASE_URL}/marks/${name}`);
